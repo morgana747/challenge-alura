9e2c527917012383d7604900c1230fa294c1059d
Milena Moreno Orozco <reina_0747@hotmail.com>
2023-04-26 08:30:02 -0500

    proyecto alura

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function encrypt() {
+    let text = document.getElementById("text").value;
+    let result = text.replace(/e/g, "enr").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    document.getElementById("result").value = result;
+    }
+
+    function decrypt() {
+        let text = document.getElementById("text").value;
+        let result = text.replace(/enr/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+        document.getElementById("result").value = result;
+    }
+
+    function copy() {
+        let result = document.getElementById("result");
+        result.select();
+        document.execCommand("copy");
+        alert("¡Texto copiado!");
+    }
